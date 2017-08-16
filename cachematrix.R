651aa86b181ff211fd5ca0c52f83d4a288af4c79
Philip Rust <philip.rust@gmail.com>
2017-08-16 17:28:53 -0400

    Second interation of homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
@@ -17,11 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
     inverse <- x$getinverse()
     if(!is.null(inverse)) {
         message("getting cached data")
